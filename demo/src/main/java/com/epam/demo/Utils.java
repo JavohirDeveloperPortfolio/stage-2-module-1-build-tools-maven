77a2ebdb781e530e773d091cca5ea4b5ce46d1d7
JavohirDeveloperPortfolio <haqberdiyev99@bk.ru>
2023-01-02 12:59:30 +0500

    again

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -3,13 +3,9 @@ package com.epam.demo;
 import com.epam.utils.StringUtils;
 
 import java.util.List;
-import java.util.function.Predicate;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       return args.stream()
-               .filter(Predicate.not(StringUtils::isPositiveNumber))
-               .findFirst()
-               .isEmpty();
+       return args.stream().allMatch(StringUtils::isPositiveNumber);
     }
 }
